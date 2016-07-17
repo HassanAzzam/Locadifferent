5cb77a4fc88ed1f153ce1f280f37b00b934c7f8b
Hassan Azzam <hassanazzam95@gmail.com>
2016-07-17 18:34:53 +0200

    first commit

---
+++ js/custom.js
@@ -0,0 +1,108 @@
+
+//slider
+(function($){
+        $(window).on("load",function(){
+            $(".content").mCustomScrollbar();
+            $(".content").mCustomScrollbar({
+           theme:"light"
+             });
+            $(".content").mCustomScrollbar({
+            axis:"x" // horizontal scrollbar
+            });
+        });
+    })(jQuery);
+
+   /* Owl Carousel
+    -----------------------------------------------*/
+  $(document).ready(function() {
+    $("#owl-work").owlCarousel({
+      autoPlay: 3000,
+      items : 3,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3],
+    });
+ /* Hide mobile menu after clicking on a link
+    -----------------------------------------------*/
+    $('.navbar-collapse a').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+  });
+
+  $(document).ready(function(){
+/*Active links
+ -----------------------------------------------*/
+    $('.list-navbar li').click(function(){
+        $(this).addClass('active').siblings().removeClass('active');
+    });
+    
+
+/*navbar background
+ -----------------------------------------------*/
+ var scrollButton=$("#scroll-top")
+ $(window).scroll(function(){
+    console.log($(this).scrollTop());
+  if ($(this).scrollTop() >=194) {
+    $(" .navbar-inverse").css("background","#464646");
+   }
+  else{
+    $(".navbar-inverse").css("background","rgba(99,99,99,0.4)");
+  }
+  if ($(this).scrollTop() >=250) {
+    scrollButton.show();
+  }
+  else{
+   scrollButton.hide();
+  }
+
+   });
+ /*click on button to scroll
+ -----------------------------------------------*/
+    scrollButton.click(function(){
+      $("html, body").animate({ scrollTop : 0},1000);
+    });
+  });
+
+ 
+
+     /*smooth scroll to section
+  -----------------------------------------------*/
+
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+    
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+      
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top-50
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('#menu-center a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('#menu-center ul li a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+}
